2fda34d9d75bb286624a6a7cf996f686e3725968
Cameron Embree <CSEmbree@gmail.com>
2014-07-01 15:46:38 +0200

    updated some steering files to be compatible with new changes to scaling

--- test_all.sh
+++ test_all.sh
@@ -2,19 +2,29 @@
 #!/bin/zsh
 #
 # just plot theory calculation with fine binning
-./plot topfinept.txt
+#./plot topfinept.txt
 
 # compare Atlas EPJ data to CMS
 ./plot atlasepjcms_top.txt
 
+
 # compare Atlas EPJ data to ATLAS new data
 ./plot atlasepjatlas.txt
 
-# compare Atlas new data to CMS data
+
+# compare Atlas new data to CMS data (PDFs: CT10 and HERA)
+#  NOTE: articially scaled CMS data in CMS data steering file
 ./plot atlascms_top.txt
+
+
+# compare Atlas new data to CMS data (NO PDFs: Grid references)
+#  NOTE: articially scaled CMS data in CMS data steering file
+./plot atlascms_top_nopdf.txt
+
 
 # compare Atlas new data to theory
 ./plot atlas2012_top.txt
+
 
 # compare Atlas new data to theory
 #./plot_old atlas2012_alphaS_top.txt
